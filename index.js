432e5a75982c9b519db069683458b445a69a3720
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-09-17 02:10:57 +0200

    Feat: EventListeners mouseover and mouseleave
    This creates EventListeners for mouseover and mouseleave events and
    change the background color on them

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const container = document.getElementById('container');
 const h1 = document.createElement('h1');
 const fragment = document.createDocumentFragment();
+let div;
+const squares = document.querySelectorAll('#container')[0].childNodes;
 
 for (let i = 0; i < 256; i++) {
-  const div = document.createElement('div');
+  div = document.createElement('div');
   div.className = 'square';
   fragment.appendChild(div);
 }
-
 
 h1.className = 'title';
 h1.textContent = 'Sketch App';
 
 container.before(h1);
 container.appendChild(fragment);
+
+squares.forEach((square, index) => square.addEventListener('mouseover', () => {
+  square.style.background = '#333';
+}));
+
+squares.forEach(square => square.addEventListener('mouseleave', () => {
+  square.style.background = '#555';
+}));
 
