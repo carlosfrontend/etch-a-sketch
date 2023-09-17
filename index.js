5007bca51a5eee59b30c10681c3f0ec659ca4bbe
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-09-18 01:57:16 +0200

    Feat: draw, createGrid and changeGrid functions

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const container = document.getElementById('container');
+const newContainer = document.getElementById('container');
 const h1 = document.createElement('h1');
 const fragment = document.createDocumentFragment();
-let div;
 const squares = document.querySelectorAll('#container')[0].childNodes;
 const buttonChangeGrid = document.createElement('button');
+const CONTAINER_WIDTH = document.getElementById('container').offsetWidth;
+let squaresNumPerSide = 16;
+let side = CONTAINER_WIDTH / squaresNumPerSide;
+let squaresNum = Math.pow(squaresNumPerSide, 2);
 
-for (let i = 0; i < 256; i++) {
-  div = document.createElement('div');
-  div.className = 'square';
-  fragment.appendChild(div);
+const draw = () => {
+
+  squares.forEach((square, index) => square.addEventListener('mouseover', () => {
+
+    square.style.background = '#333';
+
+  }));
+
+  squares.forEach(square => square.addEventListener('mouseleave', () => {
+
+    square.style.background = '#555';
+
+  }));
+
 }
+
+const createGrid = function () {
+
+  for (let i = 0; i < squaresNum; i++) {
+
+    const div = document.createElement('div');
+    div.className = 'square';
+    div.style.width = `${side / 16}em`;
+    div.style.height = `${side / 16}em`;
+    fragment.appendChild(div);
+
+  }
+
+}
+
+window.addEventListener('ready', createGrid());
 
 h1.className = 'title';
 h1.textContent = 'Sketch App';
@@ -17,19 +47,31 @@ h1.textContent = 'Sketch App';
 container.before(h1);
 container.appendChild(fragment);
 
-squares.forEach((square, index) => square.addEventListener('mouseover', () => {
-  square.style.background = '#333';
-}));
-
-squares.forEach(square => square.addEventListener('mouseleave', () => {
-  square.style.background = '#555';
-}));
-
 buttonChangeGrid.textContent = 'Change Grid';
 buttonChangeGrid.className = 'change-grid';
-buttonChangeGrid.setAttribute('onclick', 'changeGrid()' );
+buttonChangeGrid.setAttribute('onclick', 'changeGrid(fragment)');
 container.before(buttonChangeGrid);
 
 let changeGrid = () => {
+  squaresNumPerSide = Number(prompt('Enter a number'));
+  side = CONTAINER_WIDTH / squaresNumPerSide;
+  squaresNum = Math.pow(squaresNumPerSide, 2)
 
-}
+  document.querySelectorAll('.square').forEach(square => square.parentNode.removeChild(square)); //Remove squares from container
+
+  buttonChangeGrid.after(newContainer);
+
+
+  for (let i = 0; i < squaresNum; i++) {
+   
+    const newDiv = document.createElement('div');
+    newDiv.className = 'square';
+    newDiv.style.width = `${side / 16}em`;
+    newDiv.style.height = `${side / 16}em`;
+    fragment.appendChild(newDiv);
+  }
+  container.appendChild(fragment);
+  draw();
+};
+
+draw();
